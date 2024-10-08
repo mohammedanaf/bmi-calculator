213323b418282f8c047e2d592caea94ff6196ff8
mohammedanaf <mohammedanaf.developer@gmail.com>
2024-10-08 19:47:15 +0530

    Basic BMI Calculator functionality implemented

---
+++ main.py
@@ -0,0 +1,12 @@
+# Greet user with a welcome message
+print("Welcome to BMI(Body Mass Index) Calculator\n")
+
+# Ask user for their weight and height. Also; store it inside suitable variables.
+weight = float(input("What is your weight? (in kilograms)\n"))
+height = float(input("What is your height? (in meters)\n"))
+
+# Calculate the bmi score by applying necessary logic. Hint: weight divided by height squared.
+bmi = (weight / height ** 2)
+
+# Display the output on the screen
+print(bmi)
