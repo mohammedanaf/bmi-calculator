4e8f06f37b9edcd6116715571d33401ea82d8340
mohammedanaf <mohammedanaf.developer@gmail.com>
2024-10-10 15:36:19 +0530

    BMI score updated with its interpretation

--- main.py
+++ main.py
@@ -11,5 +11,17 @@ bmi = (weight / height ** 2)
 # Round the bmi score to not more than two decimal places
 bmi = round(bmi, 2)
 
-# Display the output on the screen
-print(bmi)
+# Use conditional statements and display BMI score with its interpretation
+if bmi < 18.5: # Less than 18.5
+    print(f"Your BMI score is {bmi} and you are Underweight.")
+elif bmi < 25: # Ranging between 18.5 and 25(not included)
+    print(f"Your BMI score is {bmi} and you have Healthy Weight.")
+elif bmi < 30: # Ranging between 25 and 30(not included)
+    print(f"Your BMI score is {bmi} and you are Overweight.")
+else:
+    if bmi < 35: # Ranging between 30 and 35(not included)
+        print(f"Your BMI score is {bmi} and you are Obese Class-1.")
+    elif bmi < 40: # Ranging between 35 and 40(not included)
+        print(f"Your BMI score is {bmi} and you are Obese Class-2.")
+    else: # More than 40(included)
+        print(f"Your BMI score is {bmi} and you are Obese Class-3.")
